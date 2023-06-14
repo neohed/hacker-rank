7cdb0223a5112b4731a22ede225134af66f3cb80
neohed <35173474+neohed@users.noreply.github.com>
2023-06-14 06:44:16 +0100

    Update caesar.js

--- code-wars/caesar.js
+++ code-wars/caesar.js
@@ -5,16 +5,16 @@
 const CAESAR_V = 5;
 const A_CODE = 'A'.charCodeAt(0);
 const modAlpha = n => (n % 26 + 26) % 26;
-const nShift = sign => (c, offset) => {
+const nShift = (sign, shift) => (c, offset) => {
     const normalisedCode = c.charCodeAt(0) - A_CODE;
 
     if (normalisedCode < 0 || normalisedCode > 25) {
         return c
     }
 
-    return String.fromCharCode(modAlpha(normalisedCode + (CAESAR_V + offset) * sign) + A_CODE)
+    return String.fromCharCode(modAlpha(normalisedCode + (shift + offset) * sign) + A_CODE)
 }
-const applyCipher = cipher => text => text.toUpperCase().split('').map(cipher).join('');
+const applyCipher = cipherStrategy => text => text.toUpperCase().split('').map(cipherStrategy).join('');
 const splitBuckets = (text, bucketSize) => text.split('').reduce(({buckets, part}, c, i) => {
     let newPart = part + c;
 
@@ -32,8 +32,8 @@ const splitBuckets = (text, bucketSize) => text.split('').reduce(({buckets, part
     part: ''
 });
 
-const encode = applyCipher(nShift(1));
-const decode = applyCipher(nShift(-1));
+const encode = applyCipher(nShift(1, CAESAR_V));
+const decode = applyCipher(nShift(-1, CAESAR_V));
 const plainText = 'Dave learned to paint rainbows...';
 const cipherText = encode(plainText);
 const decoded = decode(cipherText);
