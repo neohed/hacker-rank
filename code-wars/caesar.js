8d4b2f5a0ebc9668cf41f53cfec55a774675ff5e
neohed <35173474+neohed@users.noreply.github.com>
2023-06-18 08:54:05 +0100

    Update caesar.js

--- code-wars/caesar.js
+++ code-wars/caesar.js
@@ -1,20 +1,27 @@
 /*
- * https://www.codewars.com/kata/5508249a98b3234f420000fb
+ * https://www.codewars.com/kata/
+ * Perform a variation on the Caesar cipher where the letter index is added to the letter shift.
+ * Finally split the cipher text into buckets of size N.
  */
 
 const CAESAR_V = 5;
-const A_CODE = 'A'.charCodeAt(0);
-const modAlpha = n => (n % 26 + 26) % 26;
+const [A_CODE, Z_CODE, a_CODE, z_CODE] = ['A', 'Z', 'a', 'z'].map(c => c.charCodeAt(0));
+const mod26 = n => (n % 26 + 26) % 26;
 const nShift = (sign, shift) => (c, offset) => {
-    const normalisedCode = c.charCodeAt(0) - A_CODE;
+    const code = c.charCodeAt(0);
+    let base;
 
-    if (normalisedCode < 0 || normalisedCode > 25) {
-        return c
+    if (A_CODE <= code && code <= Z_CODE) {
+        base = A_CODE;
+    } else if (a_CODE <= code && code <= z_CODE) {
+        base = a_CODE;
+    } else {
+        return c;
     }
 
-    return String.fromCharCode(modAlpha(normalisedCode + (shift + offset) * sign) + A_CODE)
+    return String.fromCharCode(mod26(code - base + (shift + offset) * sign) + base);
 }
-const applyCipher = cipherStrategy => text => text.toUpperCase().split('').map(cipherStrategy).join('');
+const applyCipher = cipherStrategy => text => text.split('').map(cipherStrategy).join('');
 const splitBuckets = (text, bucketSize) => text.match(new RegExp(`.{1,${bucketSize}}`, 'g'));
 
 const encode = applyCipher(nShift(1, CAESAR_V));
